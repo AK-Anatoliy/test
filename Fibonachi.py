2da8422b62711b728d0d67dc1c675a736bf4b695
AK-Anatoliy <an.kis41@gmail.com>
2022-02-13 17:59:34 +0200

    I found the answer

--- Fibonachi.py
+++ Fibonachi.py
@@ -6,6 +6,8 @@ while a < 10:
     print(a, b)
     a, b = b, a + b
 
+print(type(a), type(b))
+
 # WHY? Will be not the same if write in two line
 print('WHY?')
 
@@ -14,3 +16,19 @@ while a < 10:
     print(a, b)
     a = b
     b = a + b
+
+'''
+https://ru.stackoverflow.com/questions/
+438740/%D1%87%D1%82%D0%BE-%D0%BE%D0%B7%D0%BD%
+D0%B0%D1%87%D0%B0%D0%B5%D1%82-a-b-b-a-b
+КОРТЕЖ
+'''
+
+print('And how it really working')
+
+a, b = 0, 1
+while a < 10:
+    print(a, b)
+    prev = a  # сохраняем предыдущее значение во временной переменной
+    a = b  # сохраняем текущее значение
+    b = prev + a  # вычисляем следующее значение как

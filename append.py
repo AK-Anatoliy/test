ec429e1a0ace1dd265a5fd221ac330a5e4af9305
AK-Anatoliy <an.kis41@gmail.com>
2022-02-14 12:54:47 +0200

    Continue

--- append.py
+++ append.py
@@ -1,3 +1,10 @@
+'''
+Important warning: The default value is evaluated only once.
+This makes a difference when the default is a mutable object such as a list,
+ dictionary, or instances of most classes. For example, the following function
+ accumulates the arguments
+ passed to it on subsequent calls:
+'''
 def f(a, L=[]):
     L.append(a)
     return L
